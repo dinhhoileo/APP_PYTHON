1e0edf595fb943bc3795e87ca9166546cd9d6f6f
dinhhoileo <huynhhoiop@gmail.com>
2024-01-27 17:44:42 +0700

    update document 21/1/2024

---
+++ 1.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+x = [12,5,7]
+y = [4,3,7]
+
+plt.plot(x,y , label = "lsgdf",color = "red")
+plt.xlabel("tiền")
+plt.ylabel("giá")
+plt.legend()
+plt.title("This is my matplotlib.pylot sđ plt")
+plt.show()
